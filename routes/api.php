17c755d54b12a6bf60ffd8257fff58ae69971081
Christopher Geary <dev@crgeary.com>
2019-07-20 23:29:35 +0100

    🤦‍♂️ Remove the other closure route. Thanks Laravel.

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Posts
 Route::middleware('auth:api')->get('/posts', 'Api\PostController@index');
 Route::middleware('auth:api')->get('/posts/{post}', 'Api\PostController@show');
